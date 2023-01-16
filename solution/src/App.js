ddc50bb9fa45afbe456680190aebc38b9f67417e
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-16 01:27:21 +0000

    App js and css is completed

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import FormComponent from "./components/form/FormComponent";
+import Contacts from "./components/contacts/Contacts";
+import { addInfo, updateInfo } from "./utils/functions";
+import "./utils/firebase";
+
+const initialValues = {
+    username: "",
+    phoneNumber: "",
+    gender: "",
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [info, setInfo] = useState(initialValues);
+
+    const handleFormSubmit = () => {
+        if (info.id) {
+            updateInfo(info);
+        } else {
+            addInfo(info);
+        }
+        setInfo(initialValues);
+    };
+
+    const handleEdit = (id, username, phoneNumber, gender) => {
+        setInfo({ id, username, phoneNumber, gender });
+    };
+
+    return (
+        <div className="App">
+            <FormComponent
+                info={info}
+                setInfo={setInfo}
+                handleFormSubmit={handleFormSubmit}
+            />
+            <Contacts handleEdit={handleEdit} />
+        </div>
+    );
 }
 
 export default App;
