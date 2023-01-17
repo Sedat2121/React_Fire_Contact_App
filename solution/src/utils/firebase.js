34b72e6a63a48d7333cc5bfeca492652ee3b747f
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-17 00:18:39 +0000

    all information pulled from firebase

--- solution/src/utils/firebase.js
+++ solution/src/utils/firebase.js
@@ -1,33 +1,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyApJXH5o1PXazmK6AU_qFsAo3gmdRdzZCI",
-    authDomain: "fire-contact-project-12.firebaseapp.com",
-    databaseURL: "https://fire-contact-project-12-default-rtdb.firebaseio.com",
-    projectId: "fire-contact-project-12",
-    storageBucket: "fire-contact-project-12.appspot.com",
-    messagingSenderId: "454240655526",
-    appId: "1:454240655526:web:32b3ad91caf88c5fa6fc67",
+    apiKey: "REACT_APP_API_KEY",
+    authDomain: "REACT_APP_AUTH_DOMAIN",
+    databaseURL: "REACT_APP_DATABASE_URL",
+    projectId: "REACT_APP_PROJECT_ID",
+    storageBucket: "REACT_APP_STORAGE_BUCKET",
+    messagingSenderId: "REACT_APP_MESSAGING_SENDER_ID",
+    appId: "REACT_APP_APP_ID",
 };
 
-// Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
 export default firebase;
-
-
-
-// import { initializeApp } from "firebase/app";
-
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_API_KEY,
-//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//     databaseURL: process.env.REACT_APP_DATABASE_URL,
-//     projectId: process.env.REACT_APP_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_APP_ID,
-// };
-
-// const firebase = initializeApp(firebaseConfig);
-
-// export default firebase;
